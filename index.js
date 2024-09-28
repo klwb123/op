4fa2e74e82fd6882cc39e90ca17a863eca1bc6eb
klwb123 <eevkt12@gmail.com>
2024-09-28 15:38:23 +0300

    index.js

--- index.js
+++ index.js
@@ -1,51 +1,26 @@
 const { Client } = require('discord.js-selfbot-v13');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
-const ytdl = require('ytdl-core');
-const express = require("express");
-
 const client = new Client();
+const express = require("express");
 const app = express();
-const listener = app.listen(process.env.PORT || 2000, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
+
+app.listen(process.env.PORT || 2000, () => {
+  console.log('Your app is listening on port ' + (process.env.PORT || 2000));
 });
 
 app.get('/', (req, res) => {
-  res.send(`
-    <body>
-      <center><h1>Bot 24H ON!</h1></center>
-    </body>
-  `);
+  res.send('<body><center><h1>Bot 24H ON!</h1></center></body>');
 });
 
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
-  const channelId = process.env.channel; // Set your channel ID here
-  const guildId = process.env.guild; // Set your guild ID here
-
-  const channel = await client.channels.fetch(channelId);
   
-  // Join voice channel
-  const connection = joinVoiceChannel({
-    channelId: channel.id,
-    guildId: guildId,
-    selfMute: false,
-    selfDeaf: false,
-    adapterCreator: channel.guild.voiceAdapterCreator
-  });
-
-  // Create audio player
-  const player = createAudioPlayer();
-  connection.subscribe(player);
-
-  // Play the YouTube video
-  const stream = ytdl('https://youtu.be/VGRQGm4-A4k', { filter: 'audioonly' });
-  const resource = createAudioResource(stream);
-
-  player.play(resource);
-
-  player.on('error', error => {
-    console.error(`Error: ${error.message}`);
-  });
+  const channel = await client.channels.fetch(process.env.channel);
+  
+  // Start screen share (replace with actual screen share logic)
+  channel.join().then(connection => {
+    // Here, you'd implement logic to share a black screen
+    console.log('Screen sharing started with black screen.');
+  }).catch(console.error);
 });
 
 client.login(process.env.token);
