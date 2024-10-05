0143543114402d6f6dbe232d37c864a9bf76a782
klwb123 <eevkt12@gmail.com>
2024-10-05 15:20:18 +0300

    index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('ready', () => {
        adapterCreator: channel.guild.voiceAdapterCreator 
        });
     }).catch((error) => { return; });
-    }, 200000)
+    }, 3000)
 }); 
 //https://ra3dstudio.com CopyRight Codes
 client.login(process.env.token);
